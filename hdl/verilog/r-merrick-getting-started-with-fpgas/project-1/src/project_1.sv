b0aac5736698b6ef1339bb6a7c860022426b4697
kris j <kris.jurkans@gmail.com>
2024-06-16 22:17:56 +0100

    project 1 done (quartus + src)

---
+++ hdl/verilog/r-merrick-getting-started-with-fpgas/project-1/src/project_1.sv
@@ -0,0 +1,18 @@
+module switches_to_leds (
+	input i_switch_1,
+	input i_switch_2,
+	input i_switch_3,
+	input i_switch_4,
+	output o_led_1,
+	output o_led_2,
+	output o_led_3,
+	output o_led_4
+);
+
+	assign o_led_1 = i_switch_1;
+	assign o_led_2 = i_switch_2;
+	assign o_led_3 = i_switch_3;
+	assign o_led_4 = i_switch_4;
+
+endmodule
+	
