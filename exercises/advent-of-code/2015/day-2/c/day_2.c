e43f8c7262277d25b60366835f19610e263d8e86
kris j <kris.jurkans@gmail.com>
2024-07-10 09:53:22 +0100

    wip aoc day 2 solution

---
+++ exercises/advent-of-code/2015/day-2/c/day_2.c
@@ -0,0 +1,22 @@
+// surface area of the box (2*l*w + 2*w*h + 2*h*l)
+// surface area of smallest side
+
+// read line from file
+// process int w, h, l 
+
+
+#include <stdlib.h>
+#include <stdio.h>
+
+#define PUZZLE_INPUT_PATH "../puzzle_input.txt"
+
+int main(void) {
+
+    printf("hello world\n");
+
+    FILE *puzzle_file = fopen(PUZZLE_INPUT_PATH, "r");
+
+
+
+    return 0;
+}
