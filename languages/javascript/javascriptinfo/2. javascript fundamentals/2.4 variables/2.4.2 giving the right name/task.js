da341a24bdcbe04aefcbdd8a5fa71455bae54912
kris j <kris.jurkans@gmail.com>
2024-08-10 15:13:03 +0100

    more javascript tasks

---
+++ languages/javascript/javascriptinfo/2. javascript fundamentals/2.4 variables/2.4.2 giving the right name/task.js
@@ -0,0 +1,5 @@
+// 1. Create a varaible with the name of our planet. How would you name such a variable?
+const PLANET_NAME_EARTH = "Earth"
+
+// 2. Create a variable to store the name of a current visitor to a website. How would you name that variable?
+let visitor_name;
---
+++ languages/javascript/javascriptinfo/2. javascript fundamentals/2.4 variables/2.4.3 uppercase const/task.js
@@ -0,0 +1,8 @@
+/* 
+    1. Would it be right to use uppercase for ``birthday``?
+        - Yes, as it is hardcoded
+
+
+    2. For age?
+        - No, since that may change depending on the date
+*/
